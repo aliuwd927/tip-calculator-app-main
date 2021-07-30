38c7916ba17aa3cf130c42a1404098d9a5bf4815
Andy Liu <aliu927@gmail.com>
2021-07-30 01:01:15 -0700

    added objects and tested. need function

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
+//Global variables
 let _billInput = document.querySelector("._billInput");
 let num_of_people = document.querySelector(".num_of_people");
 let _tipButtons = Array.from(document.querySelectorAll(".tip_button"));
-;
-let billObj = [];
+/*
+Bill Object to pass into function??
+How??
+Async and Await because of value undefined?
+ */
+let billObj = {
+    billAmount: undefined,
+    tipAmount: undefined,
+    numOfPeople: undefined
+};
 //Dynamically get input value
 _billInput === null || _billInput === void 0 ? void 0 : _billInput.addEventListener('input', () => {
     let getBillAmt = _billInput.value;
     if (getBillAmt.length > 0) {
-        //console.log(getBillAmt);
-        //billObj.billAmount = parseFloat(getBillAmt);
+        billObj.billAmount = parseFloat(getBillAmt);
     }
 });
-num_of_people === null || num_of_people === void 0 ? void 0 : num_of_people.addEventListener("input", () => {
-    let amtOfPeople = num_of_people.value;
-    if (amtOfPeople.length > 0) {
-        console.log(amtOfPeople);
-    }
-});
-//Get all buttons
+//Get the items that was clicked;
 for (let button of _tipButtons) {
     button === null || button === void 0 ? void 0 : button.addEventListener("click", (e) => {
         let tipAmt = Number(e.target.value);
-        console.log(tipAmt);
+        billObj.tipAmount = tipAmt;
     });
 }
+//Dynamically get input value
+num_of_people === null || num_of_people === void 0 ? void 0 : num_of_people.addEventListener("input", () => {
+    let amtOfPeople = num_of_people.value;
+    if (amtOfPeople.length > 0) {
+        billObj.numOfPeople = parseInt(amtOfPeople);
+    }
+});
+/*
+Async and await because intial value is undefined
+until value specified.
+*/
+const math = (x) => {
+    console.log(x);
+};
 //Math
-function calculate(amt) {
-    //Change :void to :number later
-    console.log(amt);
-}
+math(billObj);
--- index.ts
+++ index.ts
@@ -1,41 +1,55 @@
+//Global variables
 let _billInput:HTMLInputElement  =  document.querySelector("._billInput");
 let num_of_people:HTMLInputElement = document.querySelector(".num_of_people");
 let _tipButtons:Array<HTMLElement> =  Array.from(document.querySelectorAll(".tip_button"));
 
+//Set Interface Rules
 interface Bill{
-  billAmount: number;
-  numOfPeople: number;
-  tipAmount: number;
-};
+  billAmount: number,
+  tipAmount: number,
+  numOfPeople: number
+}
 
-let billObj:Bill[] = [];
+/* 
+Bill Object to pass into function?? 
+How?? 
+Async and Await because of value undefined?
+ */
+let billObj:Bill = {
+  billAmount: undefined,
+  tipAmount: undefined,
+  numOfPeople: undefined
+}
 
 //Dynamically get input value
 _billInput?.addEventListener('input', () =>{
   let getBillAmt = _billInput.value;
     if(getBillAmt.length > 0){
-      //console.log(getBillAmt);
-      //billObj.billAmount = parseFloat(getBillAmt);
+      billObj.billAmount = parseFloat(getBillAmt);
     }
 });
-
-num_of_people?.addEventListener("input", () =>{
-  let amtOfPeople = num_of_people.value;
-    if(amtOfPeople.length > 0){
-      console.log(amtOfPeople);
-    }
-});
-
-//Get all buttons
+//Get the items that was clicked;
 for(let button of _tipButtons){
   button?.addEventListener("click", (e)=>{
   let tipAmt:number = Number((e.target as HTMLButtonElement).value);
-  console.log(tipAmt);
+  billObj.tipAmount = tipAmt;
   });
 }
+//Dynamically get input value
+num_of_people?.addEventListener("input", () =>{
+  let amtOfPeople = num_of_people.value;
+    if(amtOfPeople.length > 0){
+      billObj.numOfPeople = parseInt(amtOfPeople);
+    }
+});
 
-//Math
-function calculate(amt):void{
-  //Change :void to :number later
-  console.log(amt)
+/* 
+Async and await because intial value is undefined 
+until value specified. 
+*/
+
+const math = (x):void =>{
+  console.log(x)
 }
+//Math
+math(billObj)
